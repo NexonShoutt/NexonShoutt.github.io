18c05a7110d39caeced656824c0449dceb957886
NexonShoutt <NexonShoutt@users.noreply.github.com>
2025-06-15 12:53:44 +0000

    deploy: 57d13b49665aa7d22f5ac18788e91f7008459a45

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1749992023',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
